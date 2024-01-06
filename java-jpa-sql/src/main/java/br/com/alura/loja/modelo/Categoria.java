08412737503a52707553cd07fa57366fc068731d
Caio Paiva <caiowick@hotmail.com>
2024-01-05 21:53:28 -0300

    embeddable

--- java-jpa-sql/src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ java-jpa-sql/src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -1,32 +1,28 @@
 package br.com.alura.loja.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "categorias")
 public class Categoria {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
-	
-	public Categoria() {
-	}
-	
+
+	@EmbeddedId
+	private CategoriaId id;
+
 	public Categoria(String nome) {
-		this.nome = nome;
+		this.id = new CategoriaId(nome, "xpto");
 	}
 
 	public String getNome() {
-		return nome;
+		return this.id.getNome();
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 }
